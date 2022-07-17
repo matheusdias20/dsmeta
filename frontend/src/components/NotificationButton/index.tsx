94761df368715ce1bff05ac8ea1b80b1020161f2
Matheus Vieira <mvdias20@gmail.com>
2022-07-17 18:45:51 -0300

    Send Notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,8 +1,21 @@
+import axios from 'axios';
 import icon from '../../assets/img/notification-icon.svg'
+import { BASE_URL } from '../../utils/request';
 
-export default function NotificationButton() {
+type Props = {
+    saleId: number;
+
+}
+
+function handleClick(id : number) {
+    axios(`${BASE_URL}/sales/${id}/notification`).then(response => {
+        
+    })
+}
+
+export default function NotificationButton({ saleId } : Props) {
     return (
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
             <img src={icon} alt="Notificar" />
         </div>
     )
